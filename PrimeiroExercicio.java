3f7ef2d71ede20c66e36a36b17939cb64a58741a
Pedro Lucas <108367468+pedrolucas2001@users.noreply.github.com>
2022-07-19 11:49:41 -0300

    Add files via upload

---
+++ PrimeiroExercicio.java
@@ -0,0 +1,25 @@
+package aulas;
+
+public class PrimeiroExercicio {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+
+		int numeroUm = 1;
+		int numeroDois = 20;
+		char charPedro = 'M';
+		String stringPedro = "Pedro Lucas lindo";
+		boolean verdadeiro = true;
+		boolean falso = false;
+		
+		System.out.println(numeroUm);
+		System.out.println(numeroDois);
+		System.out.println(charPedro);
+		System.out.println(stringPedro);
+		System.out.println(stringPedro + " tem "+  numeroDois+ " anos, e tem " +  numeroUm + " real");
+				
+				
+	}
+
+}
